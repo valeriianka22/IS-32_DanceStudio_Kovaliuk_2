7ce1be2a4f8e240f3e78e7f192e83e8df7f2b5bb
Valeriia Kovaliuk <lerkov22@gmail.com>
2024-10-15 00:52:56 +0300

    adapt+

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const toggleBtn = document.querySelector('.toggle-btn')
+const toggleBtnIcon = document.querySelector('.toggle-btn i')
+const dropDownMenu = document.querySelector('.dropdown-menu')
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.classList = isOpen
+        ? 'fa-solid fa-xmark'
+        : 'fa-solid fa-bars'
+}
+
+
+
+
+
+
 // const carousel = document.querySelector(".carousel"),
 // firstImg = carousel.querySelectorAll("img")[0];
 // arrowIcons = document.querySelectorAll(".wrapper i");
